7720f5e15422f01dbe7368873ead075cc3429bc7
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2023-11-03 17:56:06 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
+import user from '../json/user.json';
+import Profile from './Profile/Profile';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+import css from './Profile.module.css';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  <div className="profile">
+    <div className="description">
+      <img src={avatar} alt={username} className="avatar" />
+      <p className="name">{username}</p>
+      <p className="tag">{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{stats.followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>;
+};
+
+export default Profile;
