30a8eb3f7e167e19648413a75b2b5658f798de88
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2023-11-06 15:00:26 +0200

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,10 @@ import user from '../json/user.json';
 import Profile from './Profile/Profile';
 import data from '../json/data.json';
 import Statistic from './Statistics/Statistics';
+import friends from '../json/friends.json';
+import FriendList from './FriendList/FriendList';
+
+console.log(friends);
 
 const App = () => {
   return (
@@ -14,6 +18,7 @@ const App = () => {
         stats={user.stats}
       />
       <Statistic title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,1 +1,26 @@
 import css from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.list}>
+      {friends.map(friend => (
+        <li className={css.item} key={friend.id}>
+          <span
+            className={`${css.status} ${
+              friend.isOnline ? css.online : css.offline
+            }`}
+          ></span>
+          <img
+            className={css.avatar}
+            src={friend.avatar}
+            alt={friend.name}
+            width="48"
+          />
+          <p className={css.name}>{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
