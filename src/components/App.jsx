82cb45720adcb17cc123f23b632e2944d1ab41d7
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2023-11-06 14:16:28 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import user from '../json/user.json';
 import Profile from './Profile/Profile';
+import data from '../json/data.json';
+import Statistic from './Statistics/Statistics';
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistic title="Upload stats" stats={data} />
     </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,1 +1,20 @@
 import css from './Statistics.module.css';
+
+const Statistic = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.stat_list}>
+        {stats.map(stat => (
+          <li className={css.item} key={stat.id}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Statistic;
