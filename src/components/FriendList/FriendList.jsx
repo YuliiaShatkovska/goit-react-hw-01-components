91a6351be024ace7104d1c8d7e931c5b591c9b34
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2023-11-07 09:07:29 +0200

    fixed

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,23 +1,11 @@
 import css from './FriendList.module.css';
+import FriendListItem from 'components/FriendListItem/FriendListItem';
 
 const FriendList = ({ friends }) => {
   return (
     <ul className={css.list}>
       {friends.map(friend => (
-        <li className={css.item} key={friend.id}>
-          <span
-            className={`${css.status} ${
-              friend.isOnline ? css.online : css.offline
-            }`}
-          ></span>
-          <img
-            className={css.avatar}
-            src={friend.avatar}
-            alt={friend.name}
-            width="48"
-          />
-          <p className={css.name}>{friend.name}</p>
-        </li>
+        <FriendListItem key={friend.id} {...friend} />
       ))}
     </ul>
   );
